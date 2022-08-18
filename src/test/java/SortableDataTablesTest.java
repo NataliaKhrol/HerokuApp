f8913812b69b6298be62a35006460ae5f388dadf
NatalyaKhrol <tashapas@yahoo.com>
2022-08-18 14:16:36 +0300

    Mistakes in Sortable Data Tables Test are corrected

--- src/test/java/SortableDataTablesTest.java
+++ src/test/java/SortableDataTablesTest.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
@@ -8,19 +9,23 @@ public class SortableDataTablesTest extends BaseTest{
     @Test
     public void sortableDataTables() {
         driver.get("http://the-internet.herokuapp.com/tables");
-      //  driver.findElement(By.id("table1"));
         String personalData;
-        personalData = driver.findElement(By.xpath("//table//tr[1]//td[1]")).getText();
+        WebElement table = driver.findElement(By.id("table1"));
+        personalData = table.findElement(By.xpath("//table//tr[1]//td[1]")).getText();
         assertEquals(personalData, "Smith", "The data in the table cell doesn't match");
 
-        personalData = driver.findElement(By.xpath("//table//tr[1]//td[3]")).getText();
+        personalData = table.findElement(By.xpath("//table//tr[1]//td[3]")).getText();
         assertEquals(personalData, "jsmith@gmail.com", "The data in the table cell doesn't match");
 
-        personalData = driver.findElement(By.xpath("//table//tr[2]//td[2]")).getText();
+        personalData = table.findElement(By.xpath("//table//tr[2]//td[2]")).getText();
         assertEquals(personalData, "Frank", "The data in the table cell doesn't match");
 
-        personalData = driver.findElement(By.xpath("//table//tr[2]//td[4]")).getText();
+        personalData = table.findElement(By.xpath("//table//tr[2]//td[4]")).getText();
         assertEquals(personalData, "$51.00", "The data in the table cell doesn't match");
+
+        WebElement table2 = driver.findElement(By.id("table2"));
+        personalData = table2.findElement(By.xpath("//table//tr[1]//td[2]")).getText();
+        assertEquals(personalData, "John", "The data in the table cell doesn't match");
     }
     }
 
