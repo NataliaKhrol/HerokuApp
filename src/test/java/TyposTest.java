5ce23eab1e1118bbc57b26f54748433fa28c45e9
NatalyaKhrol <tashapas@yahoo.com>
2022-08-18 12:25:46 +0300

    Inputs Test is implemented

--- src/test/java/TyposTest.java
+++ src/test/java/TyposTest.java
@@ -5,11 +5,13 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
 
 public class TyposTest extends BaseTest {
+
     @Test
     public void typos() {
         driver.get("http://the-internet.herokuapp.com/typos");
         WebElement element = driver.findElements(By.tagName("p")).get(1);
-        String val = element.getAttribute("innerText");
-        assertEquals(val, "Sometimes you'll see a typo, other times you won,t.", "Not the same");
+        String textOnPage = element.getAttribute("innerText");
+        assertEquals(textOnPage, "Sometimes you'll see a typo, other times you won,t.",
+                "The texts are not the same");
     }
 }
