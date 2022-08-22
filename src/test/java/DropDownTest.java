4d45072f3ee9d94e974f58a4ae0653448290dd46
NatalyaKhrol <tashapas@yahoo.com>
2022-08-22 22:04:58 +0300

    DropDown Test is added with the missing task (to get all the options in the dropdown)

--- src/test/java/DropDownTest.java
+++ src/test/java/DropDownTest.java
@@ -3,6 +3,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 import static org.testng.Assert.assertTrue;
 
 public class DropDownTest extends BaseTest {
@@ -20,11 +22,21 @@ public class DropDownTest extends BaseTest {
         driver.get("http://the-internet.herokuapp.com/dropdown");
         WebElement element = driver.findElement(By.id("dropdown"));
         Select dropDown = new Select(element);
-        dropDown.selectByVisibleText("Option 1");
-        boolean isSelected = dropDown.getFirstSelectedOption().isSelected();
-        assertTrue(isSelected, "Option is not selected");
-        dropDown.selectByVisibleText("Option 2");
-        boolean isSelected2 = dropDown.getFirstSelectedOption().isSelected();
-        assertTrue(isSelected2, "Option is not selected");
+        // Select s = new Select(driver.findElement(By.xpath("//select[@name=’selType’]")));
+        // getting the list of options in the dropdown with getOptions()
+        List<WebElement> op = dropDown.getOptions();
+        int size = op.size();
+        for (int i = 0; i < size; i++) {
+            String options = op.get(i).getText();
+            System.out.println(options);
+            dropDown.selectByVisibleText("Option 1");
+            boolean isSelected = dropDown.getFirstSelectedOption().isSelected();
+            assertTrue(isSelected, "Option is not selected");
+            dropDown.selectByVisibleText("Option 2");
+            boolean isSelected2 = dropDown.getFirstSelectedOption().isSelected();
+            assertTrue(isSelected2, "Option is not selected");
+
+        }
     }
 }
+
