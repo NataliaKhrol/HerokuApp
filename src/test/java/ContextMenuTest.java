d006117f8d70e33707e978e70c577522c136d9b1
NatalyaKhrol <tashapas@yahoo.com>
2022-09-01 11:19:16 +0300

    New tests for Herokuapp are added

--- src/test/java/ContextMenuTest.java
+++ src/test/java/ContextMenuTest.java
@@ -36,10 +36,26 @@ public class ContextMenuTest extends BaseTest {
         driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
         int numberOfElements = driver.findElements(By.cssSelector("[type=checkbox]")).size();
         assertEquals(numberOfElements, 0, "Элемент присутствует на странице");
-
+        boolean isDisabled = driver.findElement(By.cssSelector("[type=text]")).isEnabled();
+        assertFalse(isDisabled, "The input is enabled");
+        driver.findElement(By.xpath("//button[text()='Enable']")).click();
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement((By.xpath("//form/p[@id='message']")))));
+        boolean isEnabled = driver.findElement(By.cssSelector("[type=text]")).isEnabled();
+        assertTrue(isEnabled, "The input is Disabled");
 
     }
+    @Test
+    public void fileUpload() {
+        driver.get("http://the-internet.herokuapp.com/upload");
+        driver.findElement(By.id("file-upload")).sendKeys("C:/Users/tasha/Desktop/Screenshot_1.png");
+        driver.findElement(By.cssSelector("[value=Upload]")).click();
+       String uploadedFile = driver.findElement(By.id("uploaded-files")).getText();
+        assertEquals(uploadedFile, "Screenshot_1.png", "er");
+    }
+
 }
    /* boolean checkbox = driver.findElement(By.cssSelector("[type=checkbox]")).is();
     assertTrue(checkbox, "Something went wrong");
+    //<p id="message">It's disabled!</p>
         driver.findElement(By.cssSelector("[disabled='']"));*/
+//*[@id="message"]
