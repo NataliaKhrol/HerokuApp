99ab2aa56d34af9be6d84210f220ecbe34bcb96c
NatalyaKhrol <tashapas@yahoo.com>
2022-09-04 22:34:44 +0300

    New test for File Downloader is added (task with asterisk)

---
+++ src/test/java/FileDownloadTest.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static org.testng.Assert.assertTrue;
+
+public class FileDownloadTest extends BaseTest{
+
+    @Test
+    public void fileDownload() {
+        driver.get("http://the-internet.herokuapp.com/download");
+        driver.findElement(By.partialLinkText("filename.txt")).click();
+        final String location = "F:\\Downloads\\filename.txt";
+        Path path = Paths.get(location);
+       // System.out.println(Files.exists(path));
+        boolean isExist = Files.exists(path);
+        assertTrue(isExist, "The file doesnot exist");
+    }
+}
